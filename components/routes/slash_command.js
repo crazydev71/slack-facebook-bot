9e402d1c9e7524b2bdb901304b4b56d7384fd1bd
Crazy Dev <crazy.dev@aol.com>
2017-08-16 17:25:45 +1000

    added direct_message, direct_mention

--- components/routes/slash_command.js
+++ components/routes/slash_command.js
@@ -2,20 +2,20 @@ var debug = require('debug')('botkit:incoming_webhooks');
 
 module.exports = function(webserver, controller) {
 
-    debug('Configured /slack/receive url');
-    webserver.post('/slack/receive', function(req, res) {
+    debug('Configured /slack/slashcommand url');
+    webserver.post('/slack/slashcommand', function(req, res) {
 
         // NOTE: we should enforce the token check here
         const message = req.body;
-
         if (message.token != process.env.verficationToken)
             return res.status(413).json({msg: 'verfication error!'});
-        
         // respond to Slack that the webhook has been received.
         res.status(200);
+        res.json({
+            // "response_type": "in_channel",
+            "text": "You just typed '" + message.text + "'.",
+            "attachments": []
+        });
         
-        // Now, pass the webhook into be processed
-        controller.handleWebhookPayload(req, res);
-
     });
 }
--- skills/slash_command.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (controller) {
-    controller.on('slash_command', function(bot, message) {
-        console.log("-------------------slash command---------------");
-        console.log(message);
-        bot.reply(message, "You just typed '" + message.text + "'.");
-        // bot.reply(message, 'Welcome, <@' + message.user + '>');
-    });
-}
