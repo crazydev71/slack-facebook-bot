deb01f5c37b8d8733e80af81d81d0ca06d02478c
Crazy Dev <crazy.dev@aol.com>
2017-08-17 23:22:38 +0200

    fixed @bot command

--- skills/event.js
+++ skills/event.js
@@ -5,7 +5,7 @@ module.exports = function (controller) {
 
 	// handler for conversation
     controller.on('direct_message,direct_mention', function(bot, message) {
-        console.log("-------------------message command---------------");
+        // console.log("-------------------message command---------------");
         console.log(message);
 		// bot.reply(message, "You just typed '" + message.text + "'.");
 		return Conversation.converse(controller.storage, bot, message);
@@ -13,19 +13,19 @@ module.exports = function (controller) {
 	
 	// handler for slash command
     controller.on('slash_command', function(bot, message) {
-        console.log("-------------------slash command----------------");
+        // console.log("-------------------slash command----------------");
         console.log(message);
         // bot.replyPrivate(message, "You just typed '" + message.text + "'.");
 
         switch(message.command) {
 			case '/lookup': // change this to your command
-				if(message.text === '') return bot.replyPrivate(message, 'Please enter a domain');
+				if(message.text === '') return bot.replyPublic(message, 'Please enter a domain');
 				return SlashCommands.lookup(controller.storage, bot, message);
 			case '/chat': // change this to your command
-				if(message.text === '') return bot.replyPrivate(message, 'Please enter a message');
+				if(message.text === '') return bot.replyPublic(message, 'Please enter a message');
 				return SlashCommands.chat(controller.storage, bot, message);
 			default:
-				bot.replyPrivate(message, `You just typed "${message.text}" using command "${message.command}"`);
+				bot.replyPublic(message, `You just typed "${message.text}" using command "${message.command}"`);
         }
     });
 }
